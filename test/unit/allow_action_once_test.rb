9ab737c446286a9c7294bd1968e855e8a95187a8
Aaron Gough <aaron@aarongough.com>
2010-05-29 15:45:36 -0400

    Removed unused test

--- test/unit/allow_action_once_test.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper.rb'))
-
-class AllowActionOnceTest < ActiveSupport::TestCase
-  
-  test "should flunk" do
-    flunk
-  end
-  
-end
