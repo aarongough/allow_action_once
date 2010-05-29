e05ab6152fe01cdda99aaf175fbde770f215746c
Aaron Gough <aaron@aarongough.com>
2010-05-29 14:58:19 -0400

    Got working test setup

--- test/unit/allow_action_once_test.rb
+++ test/unit/allow_action_once_test.rb
@@ -2,4 +2,8 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper.rb
 
 class AllowActionOnceTest < ActiveSupport::TestCase
   
+  test "should flunk" do
+    flunk
+  end
+  
 end
